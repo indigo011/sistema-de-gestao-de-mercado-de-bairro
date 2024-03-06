51985f89885f60bfd63a6b3a17fb6c8afb6b6241
Nicolas de Lima <nicolasdaniel2106@gmail.com>
2024-03-05 21:08:00 -0400

    criacao struct produto

--- apresentacao.c
+++ apresentacao.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "apresentacao.h"
 #include <locale.h>
 
 const int TAM_SEPARADOR = 40;
---
+++ produto.c
@@ -0,0 +1,5 @@
+struct produto {
+    int id, data_validade, qtd_estoque;
+    double preco;
+    char setor[21], nome[51];
+}
